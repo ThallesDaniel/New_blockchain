7d0821c3d9a961be710c348328ef56003f8c9937
ThallesDaniel <th4llesdaniel@gmail.com>
2025-03-23 01:21:28 -0300

    Início dos aprendizados de blockchain

---
+++ contracts/PersonDataStore.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4 <0.9.0;
+
+contract PersonDataStore {
+    string public name;
+    uint private age;
+
+    //não necessita informar caso seja internal
+    bool internal brazilian;
+
+
+ }
